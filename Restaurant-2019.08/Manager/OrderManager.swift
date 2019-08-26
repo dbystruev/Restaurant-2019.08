d1970759c6853cb453cab18364ca8f6f0d545137
dbystruev <github@learnswift.ru>
2019-08-26 22:58:10 +0300

    Updated order manager

---
+++ Restaurant-2019.08/Manager/OrderManager.swift
@@ -0,0 +1,23 @@
+//
+//  OrderManager.swift
+//  Restaurant-2019.08
+//
+//  Created by Denis Bystruev on 26/08/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import Foundation
+
+class OrderManager {
+    static let orderUpdatedNotification = Notification.Name("OrderManager.orderUpdated")
+    
+    static var shared = OrderManager()
+    
+    private init() {}
+    
+    var order = Order() {
+        didSet {
+            NotificationCenter.default.post(name: OrderManager.orderUpdatedNotification, object: nil)
+        }
+    }
+}
